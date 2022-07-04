28e1bd8943ab494caa90453b1c948a84c859d619
codingcalypso <101667609+codingcalypso@users.noreply.github.com>
2022-07-03 19:57:07 -0500

    Created Weather Temp js page

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
@@ -21,11 +22,9 @@ export default function WeatherInfo(props) {
               />
             </div>
             <div className="float-left">
-              <span className="temperature">
-                {Math.round(props.data.temperature)}
-              </span>
-              <span className="unit">°C</span>
+              <WeatherTemperature celsius={props.data.temperature}/>
             </div>
+          </div>
           </div>
           <div className="col-6">
             <ul>
@@ -35,6 +34,5 @@ export default function WeatherInfo(props) {
           </div>
         </div>
       </div>
-    </div>
   );
 }
