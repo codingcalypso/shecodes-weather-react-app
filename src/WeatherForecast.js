8c1d67a8abe16245595a6807ecb3cba20fa5a887
codingcalypso <101667609+codingcalypso@users.noreply.github.com>
2022-07-07 14:05:11 -0500

    Updated weather coordinates

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,17 +1,29 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
+  let apiKey = "19067ba73a1abfa7b6d138a505896e6";
+  let longitude = props.coordinates.lon;
+  let latitude = props.coordinates.lat;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+
   return (
     <div className="WeatherForecast">
       <div className="row">
         <div className="col">
           <div className="WeatherForecast-day">Thu</div>
-          <WeatherIcon code="01d" />
+          <WeatherIcon code="01d" size={36} />
           <div className="WeatherForecast-temperatures">
-            <span className="WeatherForecast=temperature-max"></span>
-            <span className="WeatherForecast=temperature-min"></span>
+            <span className="WeatherForecast-temperature-max">19°</span>
+            <span className="WeatherForecast-temperature-min">10°</span>
           </div>
         </div>
       </div>
