6d065693056c07d47e3b4b8e914be751e658b353
codingcalypso <101667609+codingcalypso@users.noreply.github.com>
2022-07-07 13:37:00 -0500

    Adding weather forecast days

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -17,22 +17,20 @@ export default function WeatherInfo(props) {
         <div className="col-6">
           <div className="clearfix mt-3">
             <div className="float-left">
-              <WeatherIcon
-                code={props.data.icon}
-              />
+              <WeatherIcon code={props.data.icon} size={52} />
             </div>
             <div className="float-left">
-              <WeatherTemperature celsius={props.data.temperature}/>
+              <WeatherTemperature celsius={props.data.temperature} />
             </div>
           </div>
-          </div>
-          <div className="col-6">
-            <ul>
-              <li>Humidity: {props.data.humidity}%</li>
-              <li>Wind: 13 km/mph</li>
-            </ul>
-          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Wind: 13 km/mph</li>
+          </ul>
         </div>
       </div>
+    </div>
   );
 }
