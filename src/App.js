71f37aff52de64774be90a24ae3762a1c8af8589
codingcalypso <101667609+codingcalypso@users.noreply.github.com>
2022-07-09 15:55:46 -0500

    Refactored code and fixed warnings

--- src/App.js
+++ src/App.js
@@ -6,15 +6,23 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="New York"/>
+        <Weather defaultCity="New York" />
         <footer>
           This project was coded by Cristal and is {""}
           <a
             href="https://github.com/codingcalypso/shecodes-weather-react-app"
             target="_blank"
-            rel="noreferrer"
+            rel="noopener noreferrer"
           >
             opened-sourced on Github
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://inquisitive-pastelito-2cdb00.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
           </a>
           .
         </footer>
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -12,6 +12,15 @@ export default function WeatherForecast(props) {
     setLoaded(true);
   }
 
+  function load() {
+    let apiKey = "19067ba73a1abfa7b6d138a505896e6";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+  }
+
   if (loaded) {
     return (
       <div className="WeatherForecast">
@@ -23,18 +32,15 @@ export default function WeatherForecast(props) {
                   <WeatherForecastDay data={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
       </div>
     );
   } else {
-    let apiKey = "19067ba73a1abfa7b6d138a505896e6";
-    let longitude = props.coordinates.lon;
-    let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-
-    axios.get(apiUrl).then(handleResponse);
+    load();
 
     return null;
   }
